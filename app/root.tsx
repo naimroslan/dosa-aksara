ebbd3d56085ac14bd56eb61a80f8590d399f23ec
naimroslan <heyitsnaim@gmail.com>
2024-02-24 01:22:31 +0800

    initial commit

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,8 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import "~/tailwind.css";
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -14,6 +16,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
       </head>
       <body>
         {children}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,30 @@
 import type { MetaFunction } from "@remix-run/node";
+import Card from "~/components/card";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Dosa Aksara" },
+    { name: "description", content: "Built by naimroslan" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="font-PressStart2P flex justify-center items-center h-screen" style={{ fontFamily: "", lineHeight: "1.8" }}>
+      <div className="flex flex-col items-center space-y-10">
+        <div className="text-4xl font-semibold text-[#121212]">
+          Dosa Aksara
+        </div>
+        <div>
+          <input className="w-96 border-black border-2 p-2.5 focus:outline-none focus:shadow-[2px_2px_0px_rgba(0,0,0,1)] focus:bg-[#3fdba7] active:shadow-[2px_2px_0px_rgba(0,0,0,1)] rounded-md"
+            placeholder="nyatakan dosa"
+          />
+        </div>
+        <div className="flex flex-row space-x-6">
+          <Card />
+          <Card />
+        </div>
+      </div>
     </div>
   );
 }
